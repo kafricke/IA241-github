939f38915f4546f7bc8b46fb44e84f19c2800f0e
EC2 Default User <ec2-user@ip-172-31-6-79.ec2.internal>
2023-02-14 21:20:19 +0000

    lec/lab4

---
+++ lec4.py
@@ -0,0 +1,23 @@
+'''
+lec 4
+dict and tuple
+'''
+my_tuple = ('a',)
+
+#my_tuple[0]='f'
+print( (my_tuple) [0:2])
+
+my_car = {         
+    'color': 'red', 
+    'maker': 'toyota', 
+    'year': 2015
+     }
+print(my_car.get('color'))
+print(my_car['color'])
+
+my_car['model']= 'venza'
+print(my_car)
+my_car['year']=2020
+print(my_car)
+
+print ('red'in my_car.values()  )
